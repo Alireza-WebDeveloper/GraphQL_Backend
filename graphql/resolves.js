2ae9a757820f2477f225086a519d4d1dbc4272c9
Alireza Khodadadi <alirezakh0199@gmail.com>
2024-05-31 13:13:25 +0330

    Add JobById Request

--- graphql/resolves.js
+++ graphql/resolves.js
@@ -1,27 +1,31 @@
 import { v4 as uuidv4 } from 'uuid';
 
+const data = [
+  {
+    id: '1',
+    title: 'programming',
+    description: 'learn javascript frameworks on 2024.',
+  },
+  {
+    id: '2',
+    title: 'student',
+    description: 'alireza are students course react.js',
+  },
+];
+
 const resolvers = {
   Query: {
     job: async () => {
-      return [
-        {
-          id: uuidv4(),
-          title: 'programming',
-          description: 'learn javascript frameworks on 2024.',
-        },
-        {
-          id: uuidv4(),
-          title: 'student',
-          description: 'alireza are students course react.js',
-        },
-      ];
+      return data;
+    },
+    jobById: (_root, args) => {
+      const { id } = args;
+      const find = data.find((item) => item.id === id);
+      if (find) return find;
+      return null;
     },
   },
   Job: {
-    title: (job) => {
-      console.log(job);
-      return ` job.title:auth-alireza`;
-    },
     company: () => {
       return {
         id: `${Math.floor(Math.random() * 21)}`,
